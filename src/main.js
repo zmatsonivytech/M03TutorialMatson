55de7a16233641f30eb1a00d1867202bc4f8d249
zmatson <113141386+zmatsonivytech@users.noreply.github.com>
2022-11-09 00:09:19 +0000

    tt

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
+/* Set up using Vue 3 */
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios';
 
-import './assets/main.css'
+import "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/js/bootstrap.bundle.min.js";
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
